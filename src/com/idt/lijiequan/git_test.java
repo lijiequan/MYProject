60b04674d398149f3315a7e874fb909786969847
lijiequan <lijiequan@gxidt.cn>
2022-05-11 18:08:09 +0800

    second commit

--- src/com/idt/lijiequan/git_test.java
+++ src/com/idt/lijiequan/git_test.java
@@ -3,5 +3,6 @@ package com.idt.lijiequan;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("hello lijiequan");
+        System.out.println("hello lijiequan2");
     }
 }
