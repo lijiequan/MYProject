1dc9f1b429e3a1dec3e2c05c7b5743a6713c8799
lijiequan <lijiequan@gxidt.cn>
2022-05-11 18:08:52 +0800

    third commit

--- src/com/idt/lijiequan/git_test.java
+++ src/com/idt/lijiequan/git_test.java
@@ -4,5 +4,7 @@ public class git_test {
     public static void main(String[] args) {
         System.out.println("hello lijiequan");
         System.out.println("hello lijiequan2");
+        System.out.println("hello lijiequan3");
+
     }
 }
